3a7891c1c9457f6922cf1b987ecc55cf91f75551
Gabriela Mitu <gabrielaym@al.insper.edu.br>
2019-11-17 19:20:12 -0300

    'firebase_e_index'

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   link.href = srtlink
 
 
-  let db = coDesConnect('https://entrega1-42708.firebaseio.com/')
+  let db = coDesConnect('https://projeto2-b7b4f.firebaseio.com/')
 
   db.download('/', function(data) {
 
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
       vai se arrepender. Você foi avisado.
     */
 
-    context = data['portfolio'][value]
+    context = data['Friseur'][value]
     console.log(context)
     coDesReplace('.csection1', context)
 
